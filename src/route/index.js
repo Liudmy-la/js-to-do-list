f16fe3c7edad5086de0112ad4554c561b58e079e
Liudmy-la <lyouda.malko@gmail.com>
2023-09-29 13:13:46 +0300

    todo-layout is done

--- src/route/index.js
+++ src/route/index.js
@@ -3,12 +3,19 @@ const express = require('express')
 const router = express.Router()
 
 // Підключіть файли роутів
-const test = require('./test')
+// const test = require('./test')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+// router.use('/', test)
 // Використовуйте інші файли роутів, якщо є
+
+router.get('/', function (req, res) {
+  res.render('index', {
+    name: 'index',
+    title: 'To-Do List',
+  })
+})
 
 // Експортуємо глобальний роутер
 module.exports = router
